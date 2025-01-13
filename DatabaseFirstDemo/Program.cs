0a51f2e5b72aaebade04aa4550a52de4d19d6a6e
Fantazindy <paulo.pete.k.razon@gmail.com>
2025-01-13 12:23:26 +0800

    used traditional coding template

--- DatabaseFirstDemo/Program.cs
+++ DatabaseFirstDemo/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace DatabaseFirstDemo
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+            Console.Read();
+        }
+    }
+}
